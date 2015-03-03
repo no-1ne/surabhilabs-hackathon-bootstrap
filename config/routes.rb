ff700c13fff4f8d3507a92432147ed5702c7cce9
Sudhakar Gariganti <sudhakar.gariganti@gmail.com>
2015-03-03 21:18:01 +0530

    Modifications based on the feedback
    - Removed the root page pointing to tutorial
    - Made authentication mandatory
    - Moved the video URL from youtube to local

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'tutorials' => 'tutorial#tutorial'
-  root :to => 'tutorial#tutorial'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
